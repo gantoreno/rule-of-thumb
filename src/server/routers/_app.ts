a357854df3edcdb41872969fd3bc65ef05537ae5
Gabriel Moreno <gantoreno@gmail.com>
2022-11-09 15:22:41 -0400

    Create `people` & `vote` procedures

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,9 +1,43 @@
+import { TRPCError } from "@trpc/server"
+import { z } from "zod"
 import { publicProcedure, router } from "../trpc"
 
 export const appRouter = router({
-  hello: publicProcedure.query(() => {
-    return "Hello, world!"
+  people: publicProcedure.query(async ({ ctx }) => {
+    const people = await ctx.prisma.person.findMany({
+      orderBy: { index: "asc" },
+    })
+
+    return people
   }),
+  vote: publicProcedure
+    .input(
+      z.object({ id: z.string(), category: z.enum(["positive", "negative"]) })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const person = await ctx.prisma.person.findFirst({
+        where: { id: input.id },
+      })
+
+      if (!person) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Person not found",
+        })
+      }
+
+      await ctx.prisma.person.update({
+        where: { id: input.id },
+        data: {
+          positiveVotes:
+            person.positiveVotes + (input.category === "positive" ? 1 : 0),
+          negativeVotes:
+            person.negativeVotes + (input.category === "negative" ? 1 : 0),
+        },
+      })
+
+      return true
+    }),
 })
 
 export type AppRouter = typeof appRouter
