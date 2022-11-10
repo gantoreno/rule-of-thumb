a0ac994b36140938511eba724d78ba7055fbe3ae
Gabriel Moreno <gantoreno@gmail.com>
2022-11-09 22:27:42 -0400

    Improve accessibility on `Navbar.tsx`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,10 @@ function Navbar() {
     <nav className="nav" role="navigation">
       <div className="max-centered">
         <h1 className="nav__logo">Rule of thumb.</h1>
-        <button className="nav__hamburger icon-button">
+        <button
+          className="nav__hamburger icon-button"
+          aria-label="Navigation menu"
+        >
           <svg width="25" height="20" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M0 0h25v4H0V0zm0 8h25v4H0V8zm0 8h25v4H0v-4z"
